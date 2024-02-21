c3fa6d5197f23bec95f22bcb44e26ee39d559605
Francisco Donaire <Franciscodonaire2a@gmail.com>
2024-02-21 01:26:33 -0300

    first commit

---
+++ server/controllers/tasks.controllers.js
@@ -0,0 +1,22 @@
+export const getTasks =  (req, res) => {
+     res.send("obteniendo tareas")
+}
+
+
+export const getTask = (req, res) => {
+    res.send("obteniendo una tareas")
+}
+
+
+export const createTask =  (req, res) => {
+    res.send("creando tareas")
+}
+
+
+export const updateTask =  (req, res) => {
+    res.send("creando tareas")
+}
+
+export const deletegetTask =  (req, res) => {
+    res.send("creando tareas")
+}
---
+++ server/routes/tasks.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get(`/tasks`)
+
+router.get(`/tasks/:id`)
+
+router.post(`/tasks`)
+
+router.put(`/tasks/:id`)
+
+router.delete(`/tasks/:id`)
